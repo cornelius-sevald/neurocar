0762e22643fe77d64ccb5f0e9f8744fc61a910e9
Cornelius Sevald-Krause <cornelius.sevald@gmail.com>
2020-01-28 22:54:03 +0100

    Make car generation CLI nicer

--- app/Main.hs
+++ app/Main.hs
@@ -1,19 +1,27 @@
 module Main where
 
-import qualified AI.GeneticAlgorithm   as GA
-import qualified AI.NeuralNetwork      as NN
-import qualified AI.NeuroCar           as NC
+import qualified AI.GeneticAlgorithm        as GA
+import qualified AI.NeuralNetwork           as NN
+import qualified AI.NeuroCar                as NC
+import           Control.Error
 import           Control.Monad
+import           Control.Monad.Extra
+import           Control.Monad.IO.Class
+import           Control.Monad.Trans.Except
 import           Data.Function
 import           Data.List
-import           Data.Word             (Word32)
+import           Data.Maybe
+import           Data.Word                  (Word32)
 import           Game
 import           System.Console.GetOpt
+import           System.Directory
 import           System.Environment
 import           System.Exit
 import           System.IO
+import           System.Random
 import           Text.Printf
-import qualified Track                 as T
+import qualified Text.Read                  as R
+import qualified Track                      as T
 import           Util
 
 data Flags
@@ -46,11 +54,33 @@ parse progName argv = case getOpt Permute flags argv of
                    exitWith (ExitFailure 1)
                where header = "Usage: " ++ progName ++ " [-gph]"
 
-prompt :: Read a => String -> IO a
-prompt str = putStr str >> hFlush stdout >> readLn
+prompt :: (Show a, Read a, MonadIO m) =>
+    String -> Maybe a -> ExceptT String m a
+prompt str defaultVal = do
+    let str' = case defaultVal of
+                 Just val -> str ++ " [" ++ show val ++ "]: "
+                 Nothing  -> str ++ ": "
+    line <- liftIO $ putStr str' >>
+                   hFlush stdout >>
+                   getLine
+    if null line
+       then case defaultVal of
+              Just val -> return val
+              Nothing  -> throwE "Empty input"
+       else case R.readMaybe line of
+              Just val -> return val
+              Nothing  -> hoistEither $ note "Invalid input"
+                                       (R.readMaybe $ "\"" ++ line ++ "\"")
 
-promptStr :: String -> IO String
-promptStr str = putStr str >> hFlush stdout >> getLine
+boundedPrompt  :: (Ord a, Show a, Read a, MonadIO m) =>
+    String -> Maybe a -> (a, a) -> ExceptT String m a
+boundedPrompt str defaultVal bound = do
+    readValue <- prompt str defaultVal
+    if readValue < fst bound || snd bound < readValue
+       then throwE $ "Value '" ++ show readValue ++
+           "' not in range [" ++ show (fst bound) ++ " : " ++
+               show (snd bound) ++ "]"
+       else return readValue
 
 main :: IO ()
 main = do
@@ -65,23 +95,28 @@ playGame :: IO ()
 playGame = putStrLn "video gaming"
 
 generateCar :: IO ()
-generateCar = do
-    seed        <- prompt    "Seed: "              :: IO Int
-    genCount    <- prompt    "Generations: "       :: IO Int
-    popSize     <- prompt    "Population size: "   :: IO Int
-    mutChance   <- prompt    "Mutation chance: "   :: IO Double
-    mutStrength <- prompt    "Mutation strength: " :: IO Double
-    deltaTicks  <- prompt    "Delta ticks: "       :: IO Word32
-    time        <- prompt    "Training time: "     :: IO Double
-    trackName   <- promptStr "Track name: "        :: IO String
-    track       <- T.fromFile $ tracksPath ++ trackName ++ ".nct"
-    let evolutions = NC.evolveCar seed genCount popSize mutChance mutStrength time deltaTicks carParams track
-    let baw = GA.bestAverageWorst evolutions
-    forM_ (zip ([0..] :: [Int]) baw) $ \(n, (b, a, w)) -> do
-        let minFit = GA.individualFitness w
-        let avgFit = a
-        let maxFit = GA.individualFitness b
-        printf "%d \t %.2f \t %.2f \t %.2f\n" n minFit avgFit maxFit
-    let bestIndividual = fst3 $ maximumBy (compare `on` GA.individualFitness . fst3) baw
-    let carName = "car-" ++ show seed
-    NN.toFile (carsPath ++ carName ++ ".nn") (GA.individualGenome bestIndividual)
+generateCar = exceptT putStrLn
+    return $ do
+        randSeed    <- flip mod 1000 <$> liftIO (randomIO :: IO Int)
+        seed        <- prompt "Seed" (Just randSeed)                                  :: ExceptT String IO Int
+        genCount    <- boundedPrompt "Generations" (Just 20) (1, 10000)               :: ExceptT String IO Int
+        popSize     <- boundedPrompt "Population size" (Just 20) (1, 10000)           :: ExceptT String IO Int
+        mutChance   <- boundedPrompt "Mutation chance" (Just 0.1) (0, 1)              :: ExceptT String IO Double
+        mutStrength <- boundedPrompt "Mutation strength" (Just 1) (0, 100)            :: ExceptT String IO Double
+        time        <- boundedPrompt "Training time" (Just 60) (0, 3600)              :: ExceptT String IO Double
+        deltaTicks  <- boundedPrompt "Delta ticks" (Just 40) (1, round $ time * 1000) :: ExceptT String IO Word32
+        trackName   <- prompt "Track name" Nothing                                    :: ExceptT String IO String
+        let trackPath = tracksPath ++ trackName ++ ".nct"
+        unlessM (liftIO $ doesFileExist trackPath) $ throwE "Invalid track name"
+        track       <- liftIO $ T.fromFile $ tracksPath ++ trackName ++ ".nct"
+        let evolutions = NC.evolveCar seed genCount popSize mutChance mutStrength time deltaTicks carParams track
+        let baw = GA.bestAverageWorst evolutions
+        forM_ (zip ([0..] :: [Int]) baw) $ \(n, (b, a, w)) -> do
+            let minFit = GA.individualFitness w
+            let avgFit = a
+            let maxFit = GA.individualFitness b
+            liftIO $ printf "%d \t %.2f \t %.2f \t %.2f\n" n minFit avgFit maxFit
+        let bestIndividual = fst3 $ maximumBy (compare `on` GA.individualFitness . fst3) baw
+        let carName = "car-" ++ show seed
+        liftIO $ createDirectoryIfMissing True carsPath
+        liftIO $ NN.toFile (carsPath ++ carName ++ ".nn") (GA.individualGenome bestIndividual)
